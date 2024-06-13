4be0cf65dba2c95a350505f27d6fbc817c687002
mo7tr <mo7tr@gmail.com>
2024-06-13 17:43:10 +0100

    first commit with setup and gh + google next-auth

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,28 @@
+import { Typography } from "@/components/ui/typography";
+import { AuthButton } from "@/features/auth/AuthButton";
+import { SiteConfig } from "@/lib/site-config";
+import Image from "next/image";
+import Link from "next/link";
+import { ThemeToggle } from "../ThemeToggle";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <div className="flex items-center gap-2">
+          <Image src="/images/logo.svg" width={50} height={35} alt="app logo" />
+          <Typography variant="h3" as={Link} href="/">
+            {SiteConfig.title}
+          </Typography>
+        </div>
+
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-1">
+            <AuthButton />
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/lib/auth.ts
@@ -0,0 +1,48 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
+import { AuthOptions, getServerSession } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { env } from "./env";
+import prisma from "./prisma";
+
+type ParametersGetServerSession =
+  | []
+  | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+  | [NextApiRequest, NextApiResponse];
+
+export const getAuthSession = async (
+  ...parameters: ParametersGetServerSession
+) => {
+  const session = await getServerSession(...parameters, authOptions);
+  return session;
+};
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  theme: {
+    logo: "/images/logo-text.png",
+  },
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_ID,
+      clientSecret: env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: env.NEXTAUTH_SECRET,
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      session.user.image = user.image;
+      return session;
+    },
+  },
+};
